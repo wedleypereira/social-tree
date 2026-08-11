36dae56243798c8e5538c4a3900acb2279baeba1
Wedley Pereira Furtado <wedley.pereira2011@hotmail.com>
2026-08-10 23:13:47 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import javascriptLogo from "./assets/javascript.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+
+document.querySelector("#app").innerHTML = ``;
